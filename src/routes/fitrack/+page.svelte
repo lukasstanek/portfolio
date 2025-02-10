c0f2e6451193055dcd26947d5b20beb981936e3c
Lukas Stanek <mail@stanek.xyz>
2025-02-10 09:12:46 +0100

    add fitrack ipa

--- src/routes/fitrack/+page.svelte
+++ src/routes/fitrack/+page.svelte
@@ -1,4 +1,12 @@
+<script>
 
+    function handleInstall(event) {
+        event.preventDefault();
+        window.location.href = 'itms-services://?action=download-manifest&url=https://stanek.xyz/manifest.plist';
+    }
+</script>
 
-<h1>Ad Hoc Hello World App Download</h1>
-<a href="itms-services://?action=download-manifest&url=https://stanek.xyz/manifest.plist">Install Application!</a>
+<div data-sveltekit-preload-data="false">
+    <h1>Ad Hoc Hello World App Download</h1>
+    <a href="#" on:click={handleInstall}>Install iOS App</a>
+</div>
